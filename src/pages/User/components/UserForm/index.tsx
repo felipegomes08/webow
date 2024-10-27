77c6beb86481262169a2e4c5a71a5181a1c07ead
felipegomes08 <felipecgomes01@hotmail.com>
2024-10-27 16:30:58 -0300

    Ajuste deploy

--- src/pages/User/components/UserForm/index.tsx
+++ src/pages/User/components/UserForm/index.tsx
@@ -11,7 +11,7 @@ const userSchema = z.object({
     .string()
     .min(1, 'O e-mail é obrigatório')
     .email('Formato de e-mail inválido'),
-  password: z.string().min(6, 'A senha precisa de no mínimo 6 caracteres')
+  password: z.string().min(6, 'A senha deve ter no mínimo 6 caracteres')
 });
 
 type UserSchema = z.infer<typeof userSchema>;
