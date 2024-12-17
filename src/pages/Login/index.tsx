5cc6a46a23579a5de4a3e2dac013e910b30e2148
felipegomes08 <felipecgomes01@hotmail.com>
2024-12-16 22:27:38 -0300

    fix: Fixed dependency not used

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,7 +2,7 @@ import { AuthContext } from 'context/AuthContext';
 import LoginBannerCard from 'pages/Login/components/LoginBannerCard';
 import LoginFormCard from 'pages/Login/components/LoginFormCard';
 import { useContext, useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export const Login = () => {
   const { authenticated, signOut } = useContext(AuthContext);
